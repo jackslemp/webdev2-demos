f18a39b825fef0c629ea3b5d893739f73cd1a2f1
jackslemp <Jackson.slemp@edu.sait.ca>
2025-01-15 13:43:40 -0700

    updated title

---
+++ app/week-2/page.js
@@ -0,0 +1,15 @@
+import Title from "./title.js";
+
+
+export default function Page(){
+    let a = 5;
+    let b = 10;
+
+    return(
+        <main>
+            <Title /> 
+            <p>This is a simple web page.</p>
+            <p>The sum of {a} and {b} is {a+b}.</p>
+        </main>
+    );
+}
